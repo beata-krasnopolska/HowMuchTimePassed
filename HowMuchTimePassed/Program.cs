1f1bc14de59ac04acea2642a1864f7bd7b752455
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-07-05 21:35:00 +0200

    Add all files

---
+++ HowMuchTimePassed/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HowMuchTimePassed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program retrieves the current date and displays how much time passed since indicated date");
+
+            DateTime currentDate = DateTime.Today;
+            DateTime birthday = new DateTime(2019, 5, 17);
+
+            var timeSpan = currentDate - birthday;
+            MonthDifferencer monthDif = new MonthDifferencer();
+            var monthsSpan = monthDif.MonthDifference(currentDate,birthday);
+
+            Console.WriteLine("Number of days since {0} to {1} : {2}", birthday.ToString("d"), currentDate.ToString("d"), timeSpan.Days);
+            Console.WriteLine(monthsSpan);
+
+            Console.ReadKey();
+        }        
+    }
+}
