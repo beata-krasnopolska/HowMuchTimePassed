a37ed392eb3798d1962f58d1264d80691db592c1
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-07-05 21:50:49 +0200

    no message

--- HowMuchTimePassed/MonthDifferencer.cs
+++ HowMuchTimePassed/MonthDifferencer.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace HowMuchTimePassed
 {
-    class MonthDifferencer
+    static class MonthDifferencer
     {
+        public static int MonthDifference(this DateTime currentDate, DateTime birthday)
+        {
+            return (currentDate.Month - birthday.Month) + 12 * (currentDate.Year - birthday.Year);
+        }
     }
 }
